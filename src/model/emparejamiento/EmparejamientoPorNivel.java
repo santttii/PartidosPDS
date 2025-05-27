9ec86bc8885473a746621acb16097b68ab0f3b7b
PatricioVecino <patriciogabrielvecino@gmail.com>
2025-05-26 21:11:14 -0300

    patron strategy

--- src/model/emparejamiento/EmparejamientoPorNivel.java
+++ src/model/emparejamiento/EmparejamientoPorNivel.java
@@ -4,29 +4,25 @@ import model.partido.Jugador;
 import model.partido.Partido;
 
 public class EmparejamientoPorNivel implements IEstrategiaEmparejamiento {
+    private Jugador.NivelJugador nivelRequerido;
 
-    public enum NivelJugador {
-        PRINCIPIANTE,
-        INTERMEDIO,
-        AVANZADO
-    }
-
-    private NivelJugador nivelJugadores;
-
-    public EmparejamientoPorNivel(NivelJugador nivelJugadores) {
+    public EmparejamientoPorNivel(Jugador.NivelJugador nivelJugadores) {
         setNivelJugadores(nivelJugadores);
     }
 
-    public NivelJugador getNivelJugadores() {
-        return nivelJugadores;
+    public Jugador.NivelJugador getNivelJugadores() {
+        return nivelRequerido;
     }
 
-    public void setNivelJugadores(NivelJugador nivelJugadores) {
-        this.nivelJugadores = nivelJugadores;
+    public void setNivelJugadores(Jugador.NivelJugador nivelJugadores) {
+        this.nivelRequerido = nivelJugadores;
     }
 
     @Override
     public boolean esApto(Jugador jugador, Partido partido) {
+        if (jugador.getNivel() == nivelRequerido) {
+            return true;
+        }
         return false;
     }
 }
